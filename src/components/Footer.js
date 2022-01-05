acb89fcf65107bf31d579ff335fce4a6a9b4344e
Drew <dreww.dev@gmail.com>
2022-01-05 16:40:05 -0500

    adjusted footer and title/ subtitle for mobile version

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,30 +2,35 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="flex-col mt-auto bg-slate-700 text-white h-auto">
-      <div className="flex lg:mx-64 my-8">
+    <div className="mt-auto bg-slate-700 text-white h-auto">
+      <div className="flex mx-12 justify-center">
         <img
-          className="w-40 h-40 mx-4 rounded-full"
+          className="w-40 h-40 mx-4 my-8 rounded-full"
           alt="Headshot of Drew Wilson"
           src="https://media-exp1.licdn.com/dms/image/C4E03AQE1pJH8HvpsYQ/profile-displayphoto-shrink_800_800/0/1633964417957?e=1646870400&v=beta&t=VqJFqMtKKB3MiNxbrnrKdtmp7TPSs_eiBgjcU5fQcK8"
         />
-        <p className="flex my-8">
-          I am a front-end web developer who enjoys building dynamic web
-          applications that are accessible and beautiful.
-        </p>
+        <div className="flex-col">
+          <p className="flex flex-wrap my-8">
+            I am a front-end web developer who enjoys building dynamic web
+            applications that are accessible and beautiful.
+          </p>
+          <ul className="">
+            <li>
+              <a className="underline" href="https://github.com/drewwdev">
+                Github
+              </a>
+            </li>
+            <li>
+              <a
+                className="underline"
+                href="https://www.linkedin.com/in/drewwdev/"
+              >
+                LinkedIn
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <ul className="mx-64">
-        <li>
-          <a className="underline" href="https://github.com/drewwdev">
-            Github
-          </a>
-        </li>
-        <li>
-          <a className="underline" href="https://www.linkedin.com/in/drewwdev/">
-            LinkedIn
-          </a>
-        </li>
-      </ul>
     </div>
   );
 }
--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Title() {
   return (
-    <div className="font-bold mx-44 mt-20 text-4xl">
+    <div className="font-bold text-center mt-20 text-4xl">
       Hello, my name is Drew.
     </div>
   );
