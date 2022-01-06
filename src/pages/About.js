8aa3a83f88163f9b95c456a3e0283ae5760e0775
Drew <dreww.dev@gmail.com>
2022-01-06 11:23:44 -0500

    About page is finished

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 function About() {
-  return <div></div>;
+  return (
+    <div className="flex-col mt-20 w-auto sm:mx-44 mx-12">
+      <p className="font-semibold my-4">About me</p>
+      <p>
+        My name is Drew Wilson. I am a front-end web developer. I am originally
+        from Oklahoma and now live in St Petersburg, Florida. I am currently
+        going to St Petersburg College where I am getting my Associate in Arts.
+        I recently graduated from Suncoast Developer's Guild where I completed
+        an intensive full stack web development program. I enjoy building web
+        applications with React, Javascript, and TailwindCSS at the moment.
+        Outside of writing code I enjoy going outside and being in nature. I
+        love hiking and exploring new trails with my wife and dog.
+      </p>
+    </div>
+  );
 }
 
 export default About;
