a1229cdd92319937a36f8212c477b05494d0fe95
Drew <dreww.dev@gmail.com>
2022-01-10 12:49:30 -0500

    refactored contact page

--- src/content/contact.js
+++ src/content/contact.js
@@ -1,3 +1,23 @@
-const contact = [{}];
+import Email from "../images/email.png";
+import LinkedIn from "../images/linkedin.png";
+import Github from "../images/github.png";
+
+const contact = [
+  {
+    src: Email,
+    href: "mailto: dreww.dev@gmail.com",
+    text: "Send me an Email",
+  },
+  {
+    src: LinkedIn,
+    href: "https://www.linkedin.com/in/drewwdev/",
+    text: "Follow me on LinkedIn",
+  },
+  {
+    src: Github,
+    href: "https://github.com/drewwdev",
+    text: "Check out my Github",
+  },
+];
 
 export default contact;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,40 +1,21 @@
 import React from "react";
-import Email from "../images/email.png";
-import LinkedIn from "../images/linkedin.png";
-import Github from "../images/github.png";
+import contact from "../content/contact";
 
 function Contact() {
   return (
     <div className="flex flex-col items-center my-20">
       <p className="font-bold text-4xl">Contact me</p>
       <ul className="flex-col">
-        <li className="flex my-8">
-          <img className="w-6 mr-4" alt="" src={Email} />
-          <a
-            className="underline hover:text-cyan-600"
-            href="mailto: dreww.dev@gmail.com"
-          >
-            Send me an Email
-          </a>
-        </li>
-        <li className="flex my-8">
-          <img className="w-6 mr-4" alt="" src={LinkedIn} />
-          <a
-            className="underline hover:text-cyan-600"
-            href="https://www.linkedin.com/in/drewwdev/"
-          >
-            Follow me on LinkedIn
-          </a>
-        </li>
-        <li className="flex my-8">
-          <img className="w-6 mr-4" alt="" src={Github} />
-          <a
-            className="underline hover:text-cyan-600"
-            href="https://github.com/drewwdev"
-          >
-            Check out my Github
-          </a>
-        </li>
+        {contact.map((data) => {
+          return (
+            <li className="flex my-8">
+              <img className="w-6 mr-4" alt="" src={data.src} />
+              <a className="underline hover:text-cyan-600" href={data.href}>
+                {data.text}
+              </a>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
